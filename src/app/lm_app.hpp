9dc325026a857e9bd2037828ba51b9be74723bb3
itdevwu <itdevwu6@gmail.com>
2022-03-19 01:49:17 +0800

    Update config

---
+++ src/app/lm_app.hpp
@@ -0,0 +1,7 @@
+#ifndef _LM_APP_HPP_
+#define _LM_APP_HPP_
+
+// LM Core headers
+#include "../core/lm_core.hpp"
+
+#endif // _LM_APP_HPP_
---
+++ src/core/io/lm_io_core.hpp
@@ -0,0 +1,11 @@
+#ifndef _LM_IO_CORE_HPP_
+#define _LM_IO_CORE_HPP_
+
+#include "../lm_core.hpp"
+
+namespace lm
+{
+    
+}
+
+#endif
